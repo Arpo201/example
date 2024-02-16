8e565c2cfda6eac7351f66c99d81b97369ee992d
Arpo201 <arpo.s201@gmail.com>
2024-02-16 17:04:09 +0700

    Restructure files, add cloud-deploy skaffold

---
+++ cloud_provider/gcp/cloud-deploy/skaffold/script.sh
@@ -0,0 +1,7 @@
+gcloud deploy apply --file 'clouddeploy.yaml' --project PROJECT_ID --region asia-southeast1
+
+# https://cloud.google.com/deploy/docs/deploying-application?hl=en#invoke_your_delivery_pipeline_to_create_a_release
+gcloud deploy releases create 'test-release-001' \
+  --project=certogo \
+  --region=asia-southeast1 \
+  --delivery-pipeline=custom-targets-pipeline
