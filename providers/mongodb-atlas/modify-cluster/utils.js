b91b02d328fea3e28ef337d42d2d7ac0e892397e
Arpo201 <sithiparp.hp@gmail.com>
2025-08-26 15:01:26 +0700

    chore: chore: mongodb trigger service handle errors

--- providers/mongodb-atlas/modify-cluster/utils.js
+++ providers/mongodb-atlas/modify-cluster/utils.js
@@ -3,24 +3,35 @@
  * See https://www.mongodb.com/docs/atlas/reference/api-resources-spec/v2/#tag/Clusters/operation/updateCluster
  *
  */
+
 exports = async function (username, password, projectID, clusterName, body) {
-  const arg = {
-    scheme: "https",
-    host: "cloud.mongodb.com",
-    path: "api/atlas/v2/groups/" + projectID + "/clusters/" + clusterName,
-    username: username,
-    password: password,
-    headers: {
-      Accept: ["application/vnd.atlas.2023-11-15+json"],
-      "Content-Type": ["application/json"],
-      "Accept-Encoding": ["bzip, deflate"],
-    },
-    digestAuth: true,
-    body: JSON.stringify(body),
-  };
+  try {
+    const arg = {
+      scheme: "https",
+      host: "cloud.mongodb.com",
+      path: "api/atlas/v2/groups/" + projectID + "/clusters/" + clusterName,
+      username: username,
+      password: password,
+      headers: {
+        Accept: ["application/vnd.atlas.2023-11-15+json"],
+        "Content-Type": ["application/json"],
+        "Accept-Encoding": ["bzip, deflate"],
+      },
+      digestAuth: true,
+      body: JSON.stringify(body),
+    };
 
-  // The response body is a BSON.Binary object. Parse it and return.
-  response = await context.http.patch(arg);
+    const response = await context.http.patch(arg);
 
-  return EJSON.parse(response.body.text());
+    if (response.statusCode !== 200) {
+      const errorBody = JSON.parse(response.body.text());
+      console.error(`Error: ${errorBody.detail || errorBody.error}`);
+      return { error: true, message: errorBody.detail || errorBody.error };
+    }
+
+    return EJSON.parse(response.body.text());
+  } catch (error) {
+    console.error("Function error:", error.message);
+    return { error: true, message: error.message };
+  }
 };
